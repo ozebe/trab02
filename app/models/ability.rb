b8684f2bfb2561a6da0b1f8d650147e14020ad1e
Wesley Ozebe <wesleysantos345@hotmail.com>
2019-06-10 23:58:14 -0300

    Ajustado permissões

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,17 +4,19 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    class Ability
-      include CanCan::Ability
-      def initialize(user)
         if user.admin?
           can :manage, :all
         else
           #can [:read, :update, :destroy], Post
           #can :manage, User
-          can :read, :all
+          #can :read, :all
+          #if post.user_id == user.id
+          #  can [:read, :update, :destroy], Post
+          #end
+          if user.present?
+            can :manage, Post, user_id: user.id
+          end
         end
       end
-    end
   end
-end
+
